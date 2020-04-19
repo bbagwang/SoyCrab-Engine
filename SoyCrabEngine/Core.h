4e85003c23920ac1d5dc100d5e41dbd8252cdc14
BBAGWANG <bbagwang@gmail.com>
2020-04-20 02:37:57 +0900

    Add Scalar Product and Vector Product

--- SoyCrabEngine/Core.h
+++ SoyCrabEngine/Core.h
@@ -9,13 +9,13 @@ public:
 	real Z;
 private:
 	//4 Word(8Byte) 맞춤을 위해 덧붙임 
-	real pad;
+	real pad = 0;
 
 public:
 	//생성, 소멸자
 	Vector3() : X(0), Y(0), Z(0) {}
 	Vector3(const real X, const real Y, const real Z) :X(X), Y(Y), Z(Z) {}
-	
+
 	//연산자
 	Vector3 operator+(const Vector3& V) const { return Vector3(X + V.X, Y + V.Y, Z + V.Z); }
 	void operator+=(const Vector3& V) { X += V.X, Y += V.Y, Z += V.Z; }
@@ -23,7 +23,9 @@ public:
 	void operator-=(const Vector3& V) { X -= V.X, Y -= V.Y, Z -= V.Z; }
 	Vector3 operator*(const real Value) const { return Vector3(X * Value, Y * Value, Z * Value); }
 	void operator *= (const real Value) { X *= Value, Y *= Value, Z *= Value; }
-	
+	//이 벡터와 주어진 벡터의 내적을 계산하여 반환한다. (ScalarProduct)
+	real operator*(const Vector3& V) const { return X * V.X + Y * V.Y + Z * V.Z; }
+
 	//부호 반전
 	void Invert()
 	{
@@ -34,10 +36,10 @@ public:
 
 	//벡터 크기 계산
 	real Magnitude() const { return real_sqrt(X * X + Y * Y + Z * Z); }
-	
+
 	//벡터 크기의 제곱 계산
 	real SquareMagnitude() const { return X * X + Y * Y + Z * Z; }
-	
+
 	//영벡터가 아닌 벡터를 단위 벡터로 변환
 	void Normalize()
 	{
@@ -45,7 +47,7 @@ public:
 		if (l > 0)
 			(*this) *= (((real)1) / l);
 	}
-	
+
 	//주어진 벡터를 주어진 스칼라만큼 더한 다음 이 벡터에 더해준다.
 	void AddScaledVector(const Vector3& Vector, real Scale)
 	{
@@ -53,7 +55,7 @@ public:
 		Y += Vector.Y * Scale;
 		Z += Vector.Z * Scale;
 	}
-	
+
 	//성분 곱셈 : 이 벡터와 주어진 벡터와의 성분 간 곱셈을 하여 그 결과를 반환한다.
 	Vector3 ComponentProduct(const Vector3& Vector) const
 	{
@@ -61,11 +63,42 @@ public:
 	}
 
 	//주어진 벡터의 성분을 이 벡터의 각 성분에 곱해준다.
-	void ComponentProjectUpdate(const Vector3& Vector)
+	void ComponentProductUpdate(const Vector3& Vector)
 	{
 		X *= Vector.X;
 		Y *= Vector.Y;
-		Z *= Vector.Z;
+		Z *= Vector.Z; 
 	}
 
-}
+	//이 벡터와 주어진 벡터의 내적을 계산하여 반환한다.
+	real ScalarProduct(const Vector3& Vector) const
+	{
+		return X * Vector.X + Y * Vector.Y + Z * Vector.Z;
+	}
+
+	//이 벡터와 주어진 벡터의 벡터곱(Vector Product)을 계산하여 반환한다.
+	//엄격한 의미로 외적은 Cross Product가 아니고 Cross Product의 크기를 의미한다. |a X b|
+	Vector3 VectorProduct(const Vector3& Vector) const
+	{
+		return Vector3(
+			Y * Vector.Z - Z * Vector.Y,
+			Z * Vector.X - X * Vector.Z,
+			X * Vector.Y - Y * Vector.X);
+	}
+
+	//이 벡터와 주어진 벡터의 벡터곱을 계산하여 반환한다.
+	Vector3 operator%(const Vector3& Vector) const
+	{
+		return Vector3(
+			Y * Vector.Z - Z * Vector.Y,
+			Z * Vector.X - X * Vector.Z,
+			X * Vector.Y - Y * Vector.X);
+	}
+
+	//이 벡터와 주어진 벡터의 벡터곱을 계산한 다음 이 벡터에 업데이트한다.
+	void operator%=(const Vector3& Vector)
+	{
+		*this = VectorProduct(Vector);
+	}
+
+};
