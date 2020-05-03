bf9298e42946e9d4ae93b48f601ea8a4a8ffa247
BBAGWANG <bbagwang@gmail.com>
2020-05-03 23:51:30 +0900

    Add Particle Class

---
+++ SoyCrabEngine/Particle.cpp
@@ -0,0 +1,147 @@
+#include "Particle.h"
+#include <assert.h>
+
+Particle::Particle()
+{
+
+}
+
+Particle::~Particle()
+{
+
+}
+
+void Particle::SetMass(const real mass)
+{
+	assert(mass != 0);
+	Particle::InverseMass = ((real)1.0) / mass;
+}
+
+real Particle::GetMass() const
+{
+	if (InverseMass == 0)
+		return REAL_MAX;
+	else
+		return ((real)1.0 / InverseMass);
+}
+
+void Particle::SetInverseMass(const real inverseMass)
+{
+	InverseMass = inverseMass;
+}
+
+real Particle::GetInverseMass() const
+{
+	return InverseMass;
+}
+
+void Particle::SetDamping(const real damping)
+{
+	Damping = damping;
+}
+
+real Particle::GetDamping() const
+{
+	return Damping;
+}
+
+void Particle::SetPosition(const Vector3& position)
+{
+	Position = position;
+}
+
+void Particle::SetPosition(const real x, const real y, const real z)
+{
+	Position.X = x;
+	Position.Y = y;
+	Position.Z = z;
+}
+
+Vector3 Particle::GetPosition() const
+{
+	return Position;
+}
+
+void Particle::GetPosition(Vector3* position) const
+{
+	*position = Position;
+}
+
+void Particle::SetVelocity(const Vector3& velocity)
+{
+	Velocity = velocity;
+}
+
+void Particle::SetVelocity(const real x, const real y, const real z)
+{
+	Velocity.X = x;
+	Velocity.Y = y;
+	Velocity.Z = z;
+}
+
+Vector3 Particle::GetVelocity() const
+{
+	return Velocity;
+}
+
+void Particle::GetVelocity(Vector3* velocity) const
+{
+	*velocity = Velocity;
+}
+
+void Particle::SetAcceleration(const Vector3& acceleration)
+{
+	Acceleration = acceleration;
+}
+
+void Particle::SetAcceleration(const real x, const real y, const real z)
+{
+	Acceleration.X = x;
+	Acceleration.Y = y;
+	Acceleration.Z = z;
+}
+
+Vector3 Particle::GetAcceleration() const
+{
+	return Acceleration;
+}
+
+void Particle::GetAcceleration(Vector3* acceleration) const
+{
+	*acceleration = Acceleration;
+}
+
+void Particle::ClearAccumulator()
+{
+	ForceAccum.Clear();
+}
+
+void Particle::AddForce(const Vector3& force)
+{
+	ForceAccum += force;
+}
+
+void Particle::Integrate(real duration)
+{
+	//무한대 질량인 입자는 적분하지 않는다.
+	if (InverseMass <= 0.f)
+		return;
+
+	assert(duration > 0.0f);
+
+	//위치를 업데이트한다.
+	Position.AddScaledVector(Velocity, duration);
+
+	//힘으로부터 가속도를 계산한다.
+	//힘이 여러 종류가 있다면, 이 벡터에 가속도를 더해준다.
+	Vector3 resultingAcc = Acceleration;
+
+	//가속도로부터 속도를 업데이트한다.
+	Velocity.AddScaledVector(resultingAcc, duration);
+
+	//드래그를 적용한다.
+	Velocity *= real_pow(Damping, duration);
+
+	//힘 항목을 지운다.
+	ClearAccumulator();
+}
---
+++ SoyCrabEngine/Particle.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "Precision.h"
+#include "Core.h"
+
+class Particle
+{
+public:
+	Particle();
+	~Particle();
+
+protected:
+	
+	//월드 공간에서 입자의 위치
+	Vector3 Position;
+	
+	//월드 공간에서 입자의 속도
+	Vector3 Velocity;
+	
+	//입자의 가속도
+	Vector3 Acceleration;
+
+	//직선 운동에 대한 댐핑(감쇄)의 정도를 저장.
+	//적분기에서 수치 연산 오차로 인해 중가한 에너지를 줄여주는 역할을 함.
+	real Damping;
+
+	//입자 질량의 역수를 저장.
+	//적분 과정이 더 간단하고 질량이 무한대인 물체(고정)가 질량이 0인 물체보다 쓰임새가 더 많으므로,
+	//입자 질량은 역수를 갖는 것이 더 편리함.
+	//(수치 시뮬레이션에서 완전히 불안정함)
+	real InverseMass;
+
+	//다음 시뮬레이션에 사용될 힘을 저장.
+	//매 이터레이션마다 0으로 초기화 된다.
+	Vector3 ForceAccum;
+
+public:
+	//질량 설정
+	void SetMass(const real mass);
+	//질량 가저오기
+	real GetMass() const;
+	//역질량 설정
+	void SetInverseMass(const real inverseMass);
+	//역질량 가저오기
+	real GetInverseMass() const;
+
+	//뎀핑(감쇄) 설정
+	void SetDamping(const real damping);
+	//뎀핑(감쇄) 가저오기
+	real GetDamping() const;
+
+	//위치 설정
+	void SetPosition(const Vector3& position);
+	void SetPosition(const real x, const real y, const real z);
+	//위치 가저오기
+	Vector3 GetPosition() const;
+	void GetPosition(Vector3* position) const;
+	//속도 설정
+	void SetVelocity(const Vector3& velocity);
+	void SetVelocity(const real x, const real y, const real z);
+	//속도 가저오기
+	Vector3 GetVelocity() const;
+	void GetVelocity(Vector3* velocity) const;
+
+	//가속도 설정
+	void SetAcceleration(const Vector3& acceleration);
+	void SetAcceleration(const real x, const real y, const real z);
+	//가속도 가저오기
+	Vector3 GetAcceleration() const;
+	void GetAcceleration(Vector3* acceleration) const;
+
+	void ClearAccumulator();
+	void AddForce(const Vector3& force);
+	//입자에 대하여, 주어진 시간 간격만큼 적분을 수행한다.
+	//이 함수는 뉴턴-오일러 적분법을 사용하는데,
+	//적분 오차를 보정하기 위해 선형 근사(Lienar Approximation)를 사용한다.
+	//그래서 때로는 부정확할 수 있다.
+	void Integrate(real duration);
+};
+
