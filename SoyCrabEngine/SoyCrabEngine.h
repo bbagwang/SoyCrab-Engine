d918c84e74e933bbdf002f43e88bcac7d5730067
BBAGWANG <bbagwang@gmail.com>
2020-05-04 18:13:11 +0900

    Add Main Operation Header

---
+++ SoyCrabEngine/SoyCrabEngine.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "app.h"
+#include "Core.h"
+#include "timing.h"
+#include "Precision.h"
+#include "Particle.h"
+#include <GL/freeglut.h>
+#include <GL/freeglut_ext.h>
+#include <GL/freeglut_std.h>
+#include <GL/glut.h>
+#include <cstring>
+#include <cstdio>
