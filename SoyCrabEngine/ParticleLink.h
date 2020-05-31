1b89b4849c92ecb0d7044febf99518a0ad472721
BBAGWANG <bbagwang@gmail.com>
2020-06-01 04:10:37 +0900

    Add Constarint Link Classes

--- SoyCrabEngine/ParticleLink.h
+++ SoyCrabEngine/ParticleLink.h
@@ -52,3 +52,58 @@ public:
 	//막대기가 늘어나거나 줄어들지 않도록 하는데 필요한 접촉을 일으킨다.
 	virtual unsigned AddContact(ParticleContact* Contact, unsigned Limit) const;
 };
+
+//컨스트레인트는 그냥 링크와 똑같지만,
+//한 가지 다른점은 움직이지 않는 앵커 포인트에 연결되어있다는 것이다.
+class ParticleConstraint : public ParticleContactGenerator
+{
+public:
+	//현재 이 컨스트레인트에 잡힌 입자들의 배열.
+	Particle* OtherParticle;
+
+	//앵커 포인트
+	Vector3 Anchor;
+
+protected:
+	//현재 링크의 길이를 반환
+	real CurrentLength() const;
+
+public:
+	//이 링크를 벗어나지 않도록 지속적으로 접촉을 생성한다.
+	//이 클래스는 단일 접점만 생성할 수 있으므로,
+	//포인터는 단일 요소에 대한 포인터가 될 수 있으며,
+	//Limit는 최소 1 (0은 유효하지 않음)로 가정되며,
+	//케이블이 과도하게 늘어나지 않은 경우 반환값은 0이고,
+	//접촉이 필요한 경우 1을 반환한다.
+	//이건 베이스 클래스고, 하위 클래스들에서 구현함.
+	virtual unsigned AddContact(ParticleContact* Contact, unsigned Limit) const = 0;
+};
+
+//컨스트레인트 케이블은 앵커 포인트로 입자를 링크해둔다.
+//너무 늘어날 경우 접촉을 발생시킨다.
+class ParticleCableConstraint : public ParticleConstraint
+{
+public:
+	//케이블의 최대 길이
+	real MaxLength;
+
+	//케이블의 반발계수 (Bounciness)
+	real Restitution;
+
+public:
+	//케이블이 너무 늘어나지 않도록, 주어진 접촉구조체에 값을 체워넣는다.
+	virtual unsigned AddContact(ParticleContact* Contact, unsigned Limit) const;
+};
+
+//컨스트레인트 막대기는 앵커 포인트로 입자를 링크해둔다.
+//너무 늘어날 경우 접촉을 발생시킨다.
+class ParticleRodConstraint : public ParticleConstraint
+{
+public:
+	//막대기의 최대 길이
+	real Length;
+
+public:
+	//막대기가 늘어나거나 줄어들지 않도록, 주어진 접촉구조체에 값을 체워넣는다.
+	virtual unsigned AddContact(ParticleContact* Contact, unsigned Limit) const;
+};
